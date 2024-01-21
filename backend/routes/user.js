34d6a990608ea16cc5ff059e2f51f4a9d10623b3
Rushi <rushikeshshelar.cs@gmail.com>
2024-01-22 00:45:37 +0530

    Add: User route to get Filtered Users

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,39 +4,43 @@ const jwt = require("jsonwebtoken");
 
 const { User } = require("../db");
 const authMiddleware = require("../middleware");
-const { generateHashedPassword, generateSalt, validatePassword } = require("../password"); // These are functions to generate salt and hashed password
 const { JWT_SECRET_TOKEN } = require("../config"); // JWT_SECRET_TOKEN is a string that you can set to anything you want. It is used to sign the JWT token.
+const {
+    generateHashedPassword,
+    generateSalt,
+    validatePassword,
+} = require("../password"); // These are functions to generate salt and hashed password
+const {
+    signUpSchema,
+    signInSchema,
+    updateSchema
+} = require("../schemas"); // Using ZOd Schema to validate Wrong inputs
 
 const router = express.Router();
-
-
-const signUpSchema = zod.object({
-    username: zod.string().min(6).max(30),
-    password: zod.string().min(8),
-    email: zod.string().email(),
-    firstName: zod.string().max(50),
-    lastName: zod.string().max(50),
-})
 
 // Path: /user/signup
 router.post("/signup", async (req, res) => {
     try {
         const body = req.body;
-        const { success} = signUpSchema.safeParse(body);
+        const { success } = signUpSchema.safeParse(body);
 
-        if(!success){
+        if (!success) {
             return res.status(411).json({
                 message: "Incorrect inputs",
             });
-        };
+        }
 
         const existingUser = await User.findOne({
-            username: body.username,
+            $or: [{
+                username: body.username,
+            }, {
+                email: body.email
+            }]
         });
 
-        if(existingUser){
+        if (existingUser) {
             return res.status(411).json({
-                message: "Email already taken / Incorrect inputs"
+                message: "Email already taken / Incorrect inputs",
             });
         }
 
@@ -55,36 +59,30 @@ router.post("/signup", async (req, res) => {
 
         const userId = user._id;
 
-        const token = jwt.sign({
-            userId,
-        }, JWT_SECRET_TOKEN);
+        const token = jwt.sign(
+            {
+                userId,
+            },
+            JWT_SECRET_TOKEN,
+        );
 
         return res.status(200).json({
             message: "User created successfully",
             token,
         });
-
-
     } catch (error) {
         console.log("[SIGNUP_ERROR]", error);
     }
 });
 
-const signInSchema = zod.object({
-    username: zod.string().min(6).max(30),
-    password: zod.string().min(8),
-});
-
 // Path: /user/signin
 router.post("/signin", async (req, res) => {
     try {
-        
-
         const body = req.body;
 
-        const { success} = signInSchema.safeParse(body);
+        const { success } = signInSchema.safeParse(body);
 
-        if(!success){
+        if (!success) {
             return res.status(411).json({
                 message: "Incorrect inputs",
             });
@@ -94,15 +92,18 @@ router.post("/signin", async (req, res) => {
             username: body.username,
         });
 
-        if(!user){
+        if (!user) {
             return res.status(411).json({
                 message: "No Users Found",
             });
         }
 
-        const isValidPassword = await validatePassword(body.password, user.password);
+        const isValidPassword = await validatePassword(
+            body.password,
+            user.password,
+        );
 
-        if(!isValidPassword){
+        if (!isValidPassword) {
             return res.status(411).json({
                 message: "Incorrect Password",
             });
@@ -110,57 +111,94 @@ router.post("/signin", async (req, res) => {
 
         const userId = user._id;
 
-        const token = jwt.sign({
-            userId,
-        }, JWT_SECRET_TOKEN);
+        const token = jwt.sign(
+            {
+                userId,
+            },
+            JWT_SECRET_TOKEN,
+        );
 
         return res.status(200).json({
             message: "User signed in successfully",
             token,
         });
-
-
     } catch (error) {
-        log.error("[SIGNIN_ERROR]", error);
+        console.log("[SIGNIN_ERROR]", error);
     }
 });
 
-const updateSchema = zod.object({
-    username: zod.string().min(6).max(30).optional(),
-    firstName: zod.string().max(50).optional(),
-    lastName: zod.string().max(50).optional(),
-})
 // Path: /user
-router.put("/" , authMiddleware ,async (req, res) => {
+router.put("/", authMiddleware, async (req, res) => {
     try {
-        
-        const { userId, body} = req;
-        
+        const { userId, body } = req;
+
         const { success } = updateSchema.safeParse(body);
 
-        if(!success){
+        if (!success) {
             return res.status(411).json({
                 message: "Incorrect inputs",
             });
         }
 
-        const user = await User.findOneAndUpdate({
-            _id: userId
-        }, {
-            username: body.username,
-            firstName: body.firstName,
-            lastName: body.lastName
-        }, {
-            new: true
-        })
+        const user = await User.findOneAndUpdate(
+            {
+                _id: userId,
+            },
+            {
+                username: body.username,
+                firstName: body.firstName,
+                lastName: body.lastName,
+            },
+            {
+                new: true,
+            },
+        );
 
         return res.status(200).json({
             user,
-            message: "User Updated Succesfuly"
-        })
-
+            message: "User Updated Succesfuly",
+        });
     } catch (error) {
         console.log("[UPDATE_USER_ERROR]", error);
+    }
+});
+
+// Path /user/bulk?filter=Rushi
+router.get("/bulk", async (req, res) => {
+    try {
+        let filter = req.query.filter || "";
+        filter = filter.toLowerCase();
+
+        const filteredUsers = await User.find({
+            $or: [
+                {
+                    firstName: {
+                        $regex: filter,
+                    },
+                },
+                {
+                    lastName: {
+                        $regex: filter,
+                    },
+                },
+                {
+                    username: {
+                        $regex: filter,
+                    },
+                },
+            ],
+        });
+
+        res.json({
+            user: filteredUsers.map((user) => ({
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                _id: user._id,
+            })),
+        });
+    } catch (error) {
+        console.log("[BULK_ERROR]", error);
     }
 });
 
