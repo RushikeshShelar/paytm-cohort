f61e85b79a30b4f349e3039fb8dcd92880d09573
Rushi <rushikeshshelar.cs@gmail.com>
2024-01-29 23:59:15 +0530

    feat: Sign Up a User

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,21 +1,80 @@
+import { useState } from "react"
+
 import { BottomWarning } from "../components/BottomWarning"
 import { Button } from "../components/Button"
 import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
+import axios from "axios"
+import { BACKEND_URL, USER_PATH } from "../../config"
+
+
 
 const Signup = () => {
-    return <div className="bg-slate-300 h-screen flex justify-center">
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [passWord, setPassWord] = useState("");
+  const [email, setEmail] = useState("");
+
+  // console.log(firstName, lastName, userName, passWord);
+
+  const resetState = () => {
+    setFirstName("");
+    setLastName("");
+    setUserName("");
+    setPassWord("");
+    setEmail("");
+  }
+
+  const handleSignUp = async () => {
+    console.log(`${BACKEND_URL}${USER_PATH}/signup`);
+    const response = await axios.post(`${BACKEND_URL}/${USER_PATH}/signup`, {
+      firstName: firstName,
+      lastName: lastName,
+      username: userName,
+      password: passWord,
+      email: email
+    });
+    localStorage.setItem("token", response.data.token);
+    resetState(); 
+  }
+
+  return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
         <Heading label={"Sign up"} />
         <SubHeading label={"Enter your infromation to create an account"} />
-        <InputBox placeholder="John" label={"First Name"} />
-        <InputBox placeholder="Doe" label={"Last Name"} />
-        <InputBox placeholder="harkirat@gmail.com" label={"Email"} />
-        <InputBox placeholder="123456" label={"Password"} />
+        <InputBox onChange={(e) => {
+          setFirstName(e.target.value)
+        }}
+          placeholder="Rushikesh"
+          label={"First Name"} />
+        <InputBox onChange={(e) => {
+          setLastName(e.target.value)
+        }}
+          placeholder="Shelar"
+          label={"Last Name"} />
+        <InputBox
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
+          placeholder="Rushi@gmail.com"
+          label={"Email"} />
+        <InputBox onChange={(e) => {
+          setUserName(e.target.value)
+        }}
+          placeholder="rushikeshShelar"
+          label={"Username"} />
+        <InputBox onChange={(e) => {
+          setPassWord(e.target.value)
+        }}
+          placeholder="123456"
+          label={"Password"} />
         <div className="pt-4">
-          <Button label={"Sign up"} />
+          <Button onClick={handleSignUp}
+            label={"Sign up"} />
         </div>
         <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
       </div>
