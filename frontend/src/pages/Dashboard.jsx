14259ec027d4ea8e280095afb502512bba227d6f
Rushi <rushikeshshelar.cs@gmail.com>
2024-01-30 02:39:14 +0530

    feat: Working Dashboard with Search

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,17 +1,38 @@
+import { useEffect, useState } from "react";
 import { AppBar } from "../components/AppBar";
 import { Balance } from "../components/Balance";
 import { Users } from "../components/Users";
+import axios from "axios";
+import { API_PATH, BACKEND_URL } from "../../config";
 
 const Dashboard = () => {
-    return ( 
+
+    const [balance, setBalance] = useState(0);
+
+    const token = localStorage.getItem("token");
+
+    useEffect(() => {
+        const getBalance = async () => {
+            const response = await axios.get(`${BACKEND_URL}/${API_PATH}/account/balance`, {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            })
+            const balance = response.data.balance;
+            setBalance(balance.toFixed(2));
+        }
+        getBalance();
+    }, [balance, setBalance, token]);
+
+    return (
         <div className="flex flex-col gap-y-4 justify-center">
             <AppBar />
             <div className="flex flex-col gap-y-3 px-10">
-            <Balance value={1000} />
-            <Users />
+                <Balance value={balance} />
+                <Users />
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Dashboard;
